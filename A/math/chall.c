2e8bceceb7b70b4d9995ea5cfd09a1b78419ab3e
huhu <tkien01102007@gmail.com>
2026-06-25 19:53:35 +0700

    mes

--- A/math/chall.c
+++ A/math/chall.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
 	char a[101];
 	scanf("%100s",a);
-	for (int i =
+	int len = strlen(a);
+	int var = 0;
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len; j++) {
+			if (a[i] == '+') {
+				i++;
+				if (a[j] == '+') {
+					j++;
+				}
+			}
+			if (a[i] < a[j]) {
+				var = a[i];
+				a[i] = a[j];
+				a[j] = var;
+			}
+		}
+	}
+	printf("%s\n", a);
 	return 0;
 }
